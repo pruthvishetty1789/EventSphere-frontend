f9b56858eda544038863c7740de0996e6d6dba87
Harshitha Prabhu <harshithaprabhu06@gmail.com>
2026-02-28 09:31:19 +0000

    Production fixes: updated nginx reverse proxy and fixed case-sensitive CSS import

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./login.css";
+import "./Login.css";
 
 function Login() {
   const navigate = useNavigate();
